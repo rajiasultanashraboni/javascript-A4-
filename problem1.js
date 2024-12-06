ab55f4ea2f3c1b57f11b305c142d29d4cbe4dbd3
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-12-06 11:35:02 +0600

    problem one is solved here

--- problem1.js
+++ problem1.js
@@ -1,9 +1,9 @@
 function calculateTax(income, expenses) {
-  if (income <= 0 && expenses <= 0 && expenses > income) {
+  if (income < 0 || expenses < 0 || expenses > income) {
     return "invalid Input";
   }
-  const income1 = 10000;
-  const expenses1 = 3000;
+  const income1 = income;
+  const expenses1 = expenses;
   remainingAmount = income1 - expenses1;
   const payTax = remainingAmount * 0.2;
   return payTax;
@@ -12,3 +12,7 @@ function calculateTax(income, expenses) {
 //test
 console.log(calculateTax(10000, 3000));
 console.log(calculateTax(34000, 1753));
+console.log(calculateTax(5000, 1500));
+console.log(calculateTax(7000, 7000));
+console.log(calculateTax(-5000, 2000));
+console.log(calculateTax(6000, -1500));
